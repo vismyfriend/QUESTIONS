20d26bb5d20ff583b35e5fac3c5d5d6bc97e80c8
Vismyfriend <vismyfriend@yahoo.com>
2023-05-30 12:10:50 +0300

    2 questions

---
+++ scripts/utils/ques-answ.js
@@ -0,0 +1,24 @@
+const questions = [
+    {
+        id: 1,
+        ru: "	1 вопросы	",
+        eng: "	1 questions	"
+    },
+    {
+        id: 2,
+        ru: "	ты агент?	",
+        eng: "	Are you an agent?	"
+    },
+]
+const answers = [
+    {
+        id: 101,
+        ru: "	Ты красавчик / красотка !",
+        eng: "	You are sexy! "
+    },
+    {
+        id: 102,
+        ru: "	What did you buy yesterday? What do you want to buy next month?",
+        eng: "	придумай вопрос с глаголом  buy "
+    },
+]	
